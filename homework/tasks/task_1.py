18d052adcbfb9c812c6331cf0873606c3362b385
kajumz <57329943+kajumz@users.noreply.github.com>
2024-12-29 22:34:06 +0300

    Update task_1.py

--- homework/tasks/task_1.py
+++ homework/tasks/task_1.py
@@ -8,9 +8,15 @@ async def await_my_func(f: Callable[..., Coroutine] | Task | Coroutine) -> Any:
 
     if isinstance(f, Callable):
         # YOUR CODE GOES HERE
+        res = await f()
+        return res
     elif isinstance(f, Task):
+        res = await f
+        return res
         # YOUR CODE GOES HERE
     elif isinstance(f, Coroutine):
+        res = await f
+        return res
         # YOUR CODE GOES HERE
     else:
         raise ValueError('invalid argument')
